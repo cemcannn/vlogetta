7d2f8a370533b35a0192efacf054dcc16711f9d0
Cem Can <cemcan@192.168.1.22>
2025-11-06 20:53:05 +0300

    Update render.yaml database reference and project configuration

--- vlogetta/settings.py
+++ vlogetta/settings.py
@@ -137,25 +137,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-# STATIC_ROOT ayarını ekleyin. Canlı ortamda (production) 'collectstatic' komutu
-# tüm statik dosyaları bu klasörde toplayacaktır.
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# WhiteNoise ayarları (Render için static file serving)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# This setting informs Django of the URI path from which your static files will be served to users
+# Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
+STATIC_URL = '/static/'
 
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
-# Render için kalıcı disk ayarı
-if os.environ.get('RENDER'):
-    MEDIA_ROOT = '/app/media'  # Render'ın mount path'i
-else:
-    MEDIA_ROOT = BASE_DIR / "media"  # Local development
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
